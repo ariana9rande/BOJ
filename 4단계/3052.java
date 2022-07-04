6a29d80d7db953ce64546f2965eda501168a4778
ariana9rande <51096442+ariana9rande@users.noreply.github.com>
2022-07-05 00:03:10 +0900

    220704

---
+++ 4단계/3052.java
@@ -0,0 +1,48 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        FastReader fr = new FastReader();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int[] arr = new int[42];
+        int count = 0;
+        
+        for(int i = 0; i < 10; i++)
+        {
+        	int input = fr.nextInt();
+        	arr[input % 42]++;
+        }
+        
+        for(int i = 0; i < arr.length; i++)
+        {
+        	if(arr[i] != 0)
+        		count++;
+        }
+        bw.write(count + "");
+        bw.flush();
+    }
+    
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public FastReader(String s) throws FileNotFoundException {
+        	br = new BufferedReader(new FileReader(new File(s))); }
+        String next() {
+        	while (st == null || !st.hasMoreElements()) {
+        		try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            } return st.nextToken(); }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str; } }
+}
