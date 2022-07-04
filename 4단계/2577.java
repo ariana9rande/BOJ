81fb459ef4bcf75f3dbd1174be5fc3672ca5e184
ariana9rande <51096442+ariana9rande@users.noreply.github.com>
2022-07-04 23:31:45 +0900

    220704
    ArrayList로 하려다가 머리 아파서 배열로 함

---
+++ 4단계/2577.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        FastReader fr = new FastReader();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int[] arr = new int[10];
+        
+        int a = fr.nextInt();
+        int b = fr.nextInt();
+        int c = fr.nextInt();
+       
+        int n = a * b * c;
+        
+        while(n > 0)
+        {
+        	arr[n % 10]++;
+        	n /= 10;
+        }
+        
+        for(int i = 0; i < arr.length; i++)
+        	bw.write(arr[i] + "\n");
+        
+        bw.flush();
+    }
+    
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public FastReader(String s) throws FileNotFoundException {
+        	br = new BufferedReader(new FileReader(new File(s))); }
+        String next() {
+        	while (st == null || !st.hasMoreElements()) {
+        		try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            } return st.nextToken(); }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str; } }
+}
