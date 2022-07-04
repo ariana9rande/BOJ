a461c357e284c7c127d5aa25fbe31349e1cb3540
ariana9rande <51096442+ariana9rande@users.noreply.github.com>
2022-07-05 03:18:20 +0900

    220704

---
+++ 6단계/11654.java
@@ -0,0 +1,38 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        FastReader fr = new FastReader();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        //문자열로 입력 받아서
+        String input = fr.next();
+        
+        //string -> int 캐스팅이 불가능하므로 첫번째 글자만 접근해서 char -> int 캐스팅
+        bw.write((int)input.charAt(0) + "");
+        bw.flush();
+    }
+    
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public FastReader(String s) throws FileNotFoundException {
+        	br = new BufferedReader(new FileReader(new File(s))); }
+        String next() {
+        	while (st == null || !st.hasMoreElements()) {
+        		try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            } return st.nextToken(); }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str; } }
+}
