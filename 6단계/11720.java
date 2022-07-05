2e9b5f3ca51b90f8c291005b2580171c3c73a0c6
ariana9rande <51096442+ariana9rande@users.noreply.github.com>
2022-07-05 21:22:26 +0900

    220705

---
+++ 6단계/11720.java
@@ -0,0 +1,44 @@
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        FastReader fr = new FastReader();
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        int n = fr.nextInt();
+        
+        String str = fr.nextLine();
+        
+        int sum = 0;
+        
+        for(int i = 0; i < str.length(); i++)
+        {
+        	sum += str.charAt(i) - '0';
+        }
+        bw.write(sum + "");
+        bw.flush();
+    }
+    
+    public static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        public FastReader() { br = new BufferedReader(new InputStreamReader(System.in)); }
+        public FastReader(String s) throws FileNotFoundException {
+        	br = new BufferedReader(new FileReader(new File(s))); }
+        String next() {
+        	while (st == null || !st.hasMoreElements()) {
+        		try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            } return st.nextToken(); }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str; } }
+}
