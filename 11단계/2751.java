5f467be360126aeb1cb5f9e42e618ea5b55fae02
ariana9rande <51096442+ariana9rande@users.noreply.github.com>
2022-07-14 00:53:00 +0900

    220714
    퀵소트 썼는데 시간초과나서 내장함수씀

--- 11단계/2751.java
+++ 11단계/2751.java
@@ -16,47 +16,14 @@ public class Main
         	int input = fr.nextInt();
         	arr.add(input);
         }
-        //시간복잡도 O(nlogn)인 퀵소트 
-        quick_sort(arr, 0, arr.size() - 1);
+        
+        Collections.sort(arr);
         
         for(int i = 0; i < arr.size(); i++)
         {
         	bw.write(arr.get(i) + "\n");
         }
         bw.flush();
-    }
-    
-    public static void quick_sort(ArrayList<Integer> arr, int start, int end)
-    {
-    	if(start >= end)
-    		return;
-    	
-    	int pivot = start;
-    	int left = start + 1;
-    	int right = end;
-    	
-    	while(left <= right)
-    	{
-    		while(left <= end && arr.get(left) <= arr.get(pivot))
-    			left++;
-    		while(right > start && arr.get(right) >= arr.get(pivot))
-    			right--;
-    		
-    		if(left > right)
-    		{
-    			int temp = arr.get(pivot);
-    			arr.set(pivot, arr.get(right));
-    			arr.set(right, temp);
-    		}
-    		else
-    		{
-    			int temp = arr.get(right);
-    			arr.set(right, arr.get(left));
-    			arr.set(left, temp);
-    		}
-    	}
-    	quick_sort(arr, start, right - 1);
-    	quick_sort(arr, right + 1, end);
     }
     
     public static class FastReader {
